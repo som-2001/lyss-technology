1e49964be3a37244c5fb02cc1475ea02208f46e3
Someswar Gorai <somgorai726@gmail.com>
2024-07-16 19:16:51 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,33 @@ import { Box, Typography } from "@mui/material";
 import "../App.css";
 import Slider from "../components/Slider";
 import { useGSAP } from "@gsap/react";
-import gsap from 'gsap';
+import { gsap } from 'gsap/dist/gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { useRef } from "react";
 
+gsap.registerPlugin(ScrollTrigger);
 
 export const Home = () => {
+
+    const scrollRef1 = useRef(null);
+
+    useGSAP(() => {
+
+        gsap.fromTo("#text00", {
+            opacity: 0,
+            y: -170
+        }, {
+            y: 0,
+            opacity: 1,
+            scrollTrigger: {
+                trigger: '#text00',
+                start: 'top bottom',
+                end: 'top 10%',
+                scrub: true
+            }
+        })
+
+    },{scope:scrollRef1});
 
     useGSAP(() => {
 
@@ -32,7 +55,7 @@ export const Home = () => {
       }}
     >
       <Navbar />
-      <Box sx={{ width: "100vw", textAlign: "center", marginTop: "100px" }} id="hero">
+      <Box sx={{ width: "100vw", textAlign: "center", marginTop: "120px" }} id="hero">
         <p style={{ fontSize: "2.3rem", fontWeight: "600" }}>Lyss Technology</p>
         <p
           style={{ marginTop: "-18px", fontSize: "1.0rem", fontWeight: "600" }}
@@ -57,8 +80,8 @@ export const Home = () => {
 
         <img src="./images/picture1.jpg" alt="" style={{ width: "280px" }} />
       </Box>
-      <Box sx={{ width: "100vw", textAlign: "center", marginTop: "100px" }}>
-        <p style={{ fontSize: "2.3rem", fontWeight: "600" }}>Our Domains</p>
+      <Box sx={{ width: "100vw", textAlign: "center", marginTop:'12rem'}}>
+        <p style={{ fontSize: "2.3rem", fontWeight: "600" }} id="text00" ref={scrollRef1}>Our Domains</p>
         
         <Slider/>
       </Box>
